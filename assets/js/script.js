af7c25a664343b1325cfeb82b79fca3d7b8594b4
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-05-19 12:21:12 +0000

    Add function to add a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
-
 // Select the elements
 
-const dateElement = document.getElementById("date");
 const dayElement = document.getElementById("day");
+const dateElement = document.getElementById("date");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+
+// Function to add task to the list
+
+function addTask() {
+    if (taskInput.value === '') {
+        alert("Please write a task in the field")
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = taskInput.value;
+        taskList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    taskInput.value = "";
+}
 
 // Show todays date
 
-const options = { month: 'long', day: 'numeric', year: 'numeric' };
-const day = { weekday: "long" };
+const options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+};
+const day = {
+    weekday: "long"
+};
 const today = new Date();
 
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 dayElement.innerHTML = today.toLocaleDateString("en-US", day);
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);

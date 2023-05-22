0c7a8870ccd3babc5ebcd4addc7eebfbeebb283f
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-05-22 08:39:52 +0000

    Add event listener to press enter to add task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,6 @@ const dateElement = document.getElementById("date");
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
 
-
-// Function to add task to the list
-
-function addTask() {
-    if (taskInput.value === '') {
-        alert("Please write a task in the field")
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = taskInput.value;
-        taskList.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    taskInput.value = "";
-}
-
 // Show todays date
 
 const options = {
@@ -36,3 +19,31 @@ const today = new Date();
 
 dayElement.innerHTML = today.toLocaleDateString("en-US", day);
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+
+// Function to add task to the list
+
+document.getElementById("task-input").focus();
+
+document.getElementById("task-input").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+})
+
+function addTask() {
+
+    if (taskInput.value === '') {
+        alert("Please write a task in the field")
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = taskInput.value;
+        taskList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    
+    taskInput.value = "";
+    
+}
